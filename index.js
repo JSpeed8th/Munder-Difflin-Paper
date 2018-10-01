4cfcd1e478eabf1baff38137d89de17f66d4cc63
Jordan <jspeed8th@gmail.com>
2018-10-01 14:46:50 -0400

    added js functionality to navbar to reveal a fourth link.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var title = document.getElementsByClassName('title')[0];
-
+// var navBar = document.getElementsByClassName('.navbar')
+// var listedItems = document.getElementsByTagName('li')
+// var list = document.getElementsByTagName('ul')[0];
+var newLi = document.getElementById('newLi');
 
 
 title.addEventListener('mouseover', function() {
@@ -9,7 +12,22 @@ title.addEventListener('mouseover', function() {
 });
 
 title.addEventListener('mouseout', function() {
-  title.style.transition = '1s';
+  // title.style.transition = '1s';
   title.style.color = '#92D5E6';
   title.innerHTML = 'the blueview';
+});
+
+
+// ------------------------
+window.addEventListener('scroll', function() {
+  var scrolled = window.scrollY;
+  console.log(scrolled);
+  if (scrolled >= 645) {
+    newLi.style.transition = '1.5s';
+    newLi.style.fontSize = '1em';
+    newLi.style.opacity = '1';
+  } else if(scrolled < 645) {
+    newLi.style.fontSize = '0';
+    newLi.style.opacity = '.0';
+  }
 });
