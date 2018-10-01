4dd7d8da90f17f622bb82ab49494bd6b4c8ec452
Jordan <jspeed8th@gmail.com>
2018-09-30 21:21:50 -0400

    completed compare section

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var title = document.getElementsByClassName('title')[0];
 
 title.addEventListener('mouseover', function() {
-  title.style.color = '#1D3942';
   title.style.transition = '1s';
+  title.style.color = '#1D3942';
+  title.textContent = 'Hello World';
 })
 title.addEventListener('mouseout', function() {
+  title.style.transition = '1s';
   title.style.color = '#009FDF';
+  title.textContent = 'the blueview';
 })
