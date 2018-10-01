1425740b58e3014414bff9810d1411610c70288c
Jordan <jspeed8th@gmail.com>
2018-09-30 21:47:53 -0400

    completed project. May add additions later on.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var title = document.getElementsByClassName('title')[0];
+var motto = document.querySelector('.motto');
 
 title.addEventListener('mouseover', function() {
   title.style.transition = '1s';
   title.style.color = '#1D3942';
-  title.textContent = 'Hello World';
-})
+  title.innerHTML = 'hello world';
+});
 title.addEventListener('mouseout', function() {
   title.style.transition = '1s';
-  title.style.color = '#009FDF';
-  title.textContent = 'the blueview';
-})
+  title.style.color = '#92D5E6';
+  title.innerHTML = 'the blueview';
+});
